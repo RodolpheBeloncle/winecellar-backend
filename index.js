2c2c9002d34e646c7c844d0e21ed315e72383e7f
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-30 12:21:39 +0200

    allow header

--- index.js
+++ index.js
@@ -24,11 +24,18 @@ mongoose
     console.log(err);
   });
 
-const corsOptions = {
+
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+    });
+  
+  const corsOptions = {
   origin: process.env.CLIENT_URL,
   credentials: true,
 };
-
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
