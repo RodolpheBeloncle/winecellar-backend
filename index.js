c51f65551ec9d6654dd527a3c55e235d3ddd679d
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-30 15:02:20 +0200

    change cors

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ mongoose
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
