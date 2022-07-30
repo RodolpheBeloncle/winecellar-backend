2b0e019f39aa4ce9647318fe321581659fc14242
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-30 15:13:52 +0200

    remove cors

--- index.js
+++ index.js
@@ -24,20 +24,7 @@ mongoose
     console.log(err);
   });
 
-
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-  
-  const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
