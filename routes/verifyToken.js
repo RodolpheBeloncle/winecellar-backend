d3cd314e2c633f2854d0ceb75ede0d4075fd1519
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-31 18:29:23 +0200

    fix

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const cookies = ctx.request.header.cookie || false;
+  const cookies = req.header.cookie || false;
   if (cookies) {
     let token = cookies
       .split(';')
       .find((c) => c.trim().startsWith('token='))
       .split('=')[1];
     if (token) {
-      ctx.request.header.authorization = `Bearer ${token}`;
+      req.header.authorization = `Bearer ${token}`;
     }
   }
   next();
