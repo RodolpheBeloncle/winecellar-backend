14f0f5cccfd578cb2ff951cd64e00539b9280c67
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-31 17:51:58 +0200

    fix erros

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = async (req, res, next) => {
-  const authHeader = req.cokkies.token;
+  const authHeader = req.cookies.token;
 
   if (authHeader) {
     jwt.verify(authHeader, process.env.TOKEN_SECRET, (err, user) => {
       if (!user) {
         return res.sendStatus(403);
       }
-      next();
+      console.log("token cookies",user)
+      return next();
     });
   } else {
     res.sendStatus(401);
