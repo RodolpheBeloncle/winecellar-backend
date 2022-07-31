e55826d712f46db381121b3ecc208d00d7e232e0
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-31 16:47:18 +0200

    resolve token middelware

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next) => {
-  const authToken = req.headers.cookie;
-
-  if (!authToken) {
-    return res.status(403).json({ message: 'Token is not valid!' });
-  }
-  jwt.verify(authToken, process.env.TOKEN_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: 'You are not authenticated!' });
+const verifyToken = async (req, res, next) => {
+  try {
+    if (!req.headers.cookie) {
+      res.status(403).json({ message: 'Token is not valid!' });
     }
-    next();
-
-  });
+    jwt.verify(authToken, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.status(401).json({ message: 'You are not authenticated!' });
+      }
+      res.status(200).cookie('token', user)
+      next();
+    });
+  } catch (error) {
+    return res.status(401).json({ message: error });
+  }
 };
 
 module.exports = {
