9c9977a80a8416bf9c9a1a0a95c916ed69ae2daa
RodolpheBeloncle <rbeloncle@gmail.com>
2022-09-02 17:14:41 +0200

    clear cors

--- index.js
+++ index.js
@@ -33,17 +33,17 @@ app.use(
 //   'https://winecellar.rodolphebeloncle.com',
 //   'http://localhost:3000',
 // ];
-let corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-app.use(cors(corsOptions));
+// let corsOptions = {
+//   credentials: true,
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
