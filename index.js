90acb6757e9ccfa59c84ca4b37c26f59be4cc5e2
RodolpheBeloncle <rbeloncle@gmail.com>
2022-09-03 08:27:37 +0200

    setting deployment mode

--- index.js
+++ index.js
@@ -25,25 +25,25 @@ mongoose
   });
 
 app.use(cookieParser());
-// app.use(
-//   cors({ credentials: true, origin: 'https://winecellar.rodolphebeloncle.com' })
-// );
+app.use(
+  cors({ credentials: true, origin: 'https://winecellar.rodolphebeloncle.com' })
+);
 
-let whitelist = [
-  'https://winecellar.rodolphebeloncle.com',
-  'http://localhost:3000',
-];
-let corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-app.use(cors(corsOptions));
+// let whitelist = [
+//   'https://winecellar.rodolphebeloncle.com',
+//   'http://localhost:3000',
+// ];
+// let corsOptions = {
+//   credentials: true,
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
